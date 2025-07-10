a0f8cd8435e4a265e6512531b02963d48e7c50ac
dammar01 <dammar.s011@gmail.com>
2025-07-10 07:45:14 +0700

    init

---
+++ public/js/buat-jadwal.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+  $("#example").DataTable({
+    dom:
+      "<'row align-items-center'" +
+      "<'col-auto'<'row d-flex flex-row align-items-center justify-content-start' <'col-auto showdata 'l><'col-auto mb-0'B>>>" +
+      "<'col d-flex align-items-center justify-content-end searchdata mb-2'f>" +
+      ">" +
+      "<'table-responsive mb-2'tr>" +
+      "<'row justify-content-between'" +
+      "<'col-md-6 ps-2'i>" +
+      "<'col-md-6 pe-2'p>" +
+      ">",
+    language: {
+      lengthMenu: "_MENU_",
+      processing: `
+                <div class="container-spinner">
+                    <div class="spinner"></div>
+                    <p class="pt-3">Loading ...</p>
+                </div>`,
+      zeroRecords: `
+                <p style="font-size: 15px; font-weight: 500; margin-top:10px; margin-bottom: 5%;">Data Belum Tersedia</p>`,
+      search: "",
+      searchPlaceholder: "Search",
+    },
+    destroy: true,
+    responsive: true,
+    pageLength: 10,
+    lengthMenu: [
+      [10, 25, 50, -1],
+      [10, 25, 50, "All"],
+    ],
+  });
+});
