6cf733d1384e64914c936a96f2799077b0bd2602
dammar01 <dammar.s011@gmail.com>
2025-07-12 13:50:37 +0700

    penyesuiaian jadwal saya

---
+++ public/js/auth.js
@@ -0,0 +1,41 @@
+const data_user = [
+  {
+    nama: "Dammar Syaputra",
+    username: "3202316085",
+    password: "asda",
+    role: "mahasiswa",
+  },
+  {
+    nama: "Haikal Bagas Putra",
+    username: "3202316104",
+    password: "asda",
+    role: "mahasiswa",
+  },
+  {
+    nama: "Haikal Bagas Putra",
+    username: "3202316104",
+    password: "asda",
+    role: "mahasiswa",
+  },
+  {
+    nama: "Suharsono",
+    username: "0001",
+    password: "asda",
+    role: "dosen",
+  },
+  {
+    nama: "Satriyo",
+    username: "0002",
+    password: "asda",
+    role: "dosen",
+  },
+];
+
+const auth = localStorage.getItem("auth");
+if (auth) {
+  const parsed_user = JSON.parse(auth);
+  if (parsed_user.role === "mahasiswa") window.location.href = "../../mahasiswa/dashboard.html";
+  else window.location.href = "../../dosen/dashboard.html";
+} else {
+  window.location.href = "../../login.html";
+}
